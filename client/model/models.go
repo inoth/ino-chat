276696271f5b888a8cf6d4177e7064fd3fda989e
inoth <inoth@DESKTOP-O2RNFEC>
2021-11-03 11:06:32 +0800

    .

--- client/model/models.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type MessageNsqBody struct {
-	MsgType  int      `json:"msgType"`
-	Target   []string `json:"target"`
-	FromUser string   `json:"fromUser"`
-	Body     string   `json:"body"`
-}
